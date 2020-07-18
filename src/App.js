d286de9600e7e2d0c4361c6cd5a24db34a7edb29
George Flores <georgeflores886@gmail.com>
2020-07-18 10:29:54 -0400

    implemented routes in server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import FileUpload from './components/FileUpload'
+import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <h1>React File Upload Demo</h1>
+      <FileUpload/>
     </div>
   );
 }
